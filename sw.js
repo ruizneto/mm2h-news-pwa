aab0d39c3468d94eaf4a63ecdf0338b33f66723b
ruizneto <sangjuarakl@gmail.com>
2025-12-25 09:11:29 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
